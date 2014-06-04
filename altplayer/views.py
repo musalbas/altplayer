9db6377f1269d78b3af1c143b6bab4fbc9ba79a0
Mustafa <mus@musalbas.com>
2014-06-04 16:12:34 +0100

    Template base

--- altplayer/views.py
+++ altplayer/views.py
@@ -1,7 +1,9 @@
+from flask import render_template
+
 from altplayer import app
 from altplayer import iplayer
 from altplayer import programmes
 
 @app.route('/programme/<pid>')
 def view_programme(pid):
-    return str(programmes[pid])
+    return render_template('programme.html', programme=programmes[pid])
